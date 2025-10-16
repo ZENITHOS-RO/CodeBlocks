6f1ae50824a86387d664e5625db74abc30d18e25
ZENITHOS <superproinf@gmail.com>
2025-10-16 10:36:31 +0800

    Update Framework.lua

--- Framework.lua
+++ Framework.lua
@@ -1,6 +1,27 @@
 local export = {}
-local setups = {}
-local essentials = {}
-local processors = {}
+local this = {}
+local setup = {}
+local essential = {}
+local processor = {}
+
+local clonables = {
+  Block = nil,
+  ProcedureBlock = nil,
+  VariableBlock = nil
+}
+
+function this.phraseBlockType (connections:{})
+  if connections.CanInsert then
+    return "VariableBlock"
+  elseif connections.CanStack then
+    return "ProcedureBlock"
+  else
+    return "Block"
+  end
+end
+
+function processor:render (BlockRenderData:{})
+
+end
 
 return export
