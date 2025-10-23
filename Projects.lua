e269ee69474cc7a8d61a43c916bfd885aae31aa9
ZENITHOS <superproinf@gmail.com>
2025-10-23 15:35:32 +0800

    Implement loopThruRender for project rendering
    Added functionality to loop through and render project blocks.

--- Projects.lua
+++ Projects.lua
@@ -1,7 +1,30 @@
 local export = {}
+local this = {}
 
-function export:loadProject(ProjectData:{})
+local current, progresses = 0, 0
+
+function this.loopThruRender (StartingInstance:Frame, CurrentIndex:string, BlockData:{})
+  for colidx, coldata in pairs(BlockData) do
+      --render
+      if coldata["stack"] then
+        for tabidx, tabdata in pairs(coldata["stack"] do
+          
+        end
+      end
+  end
+end
+
+function export:loadProject (ProjectModule:ModuleScript)
+  if typeof(ProjectModule) ~= "ModuleScript" then
   
+  end
+
+  local pm = require(ProjectModule)
+  for groupidx, groupdata in pairs(pm.codes) do
+    if not BlockData["position"] then return -2 end
+    --call render for first Block
+
+  end
 end
 
 return export
